09e9d8af9b241032fe1996e8bfd41fb7591d3ff6
MichalDziedzic <michaldziedzic8@gmail.com>
2021-07-15 17:28:28 +0200

    add ipify api to get location user data

--- app/src/App.js
+++ app/src/App.js
@@ -1,9 +1,7 @@
-import React from "react";
+import React, {useState,useEffect} from "react";
+import axios from 'axios';
 import './App.css';
-
-//import { Icon } from "leaflet";
-// import * as parkData from "./data/skateboard-parks.json";
- import { MapContainer, TileLayer,Marker,Popup} from 'react-leaflet'
+import { MapContainer, TileLayer,Marker,Popup} from 'react-leaflet'
 
 import HeaderTitle from'./components/Header';
 import InputTracker from'./components/InputTracker';
@@ -11,9 +9,20 @@ import ResultTracker from'./components/ResultTracker';
 // import Map from './components/Map';
 
  const App=()=> {
+     const [ipUserInfo ,setIpUserInfo]=useState([]);
+    const apiKey=`at_3IpXga8xWzOhB4m0Jta5UyQgtFgLW`;
+
+  useEffect(()=>{
+    axios.get(`https://geo.ipify.org/api/v1?apiKey=${apiKey}&ipAddress=8.8.8.8`)
+    .then(response=>setIpUserInfo(response))
+
+  },)
   const projectAdress = `https://www.frontendmentor.io?ref=challenge`;
    const author= `Michał Dziedzic`;
   const position = [51.505, -0.09]
+
+
+
    return(
   <div>
    <header>
@@ -25,14 +34,14 @@ import ResultTracker from'./components/ResultTracker';
       {/* <Map></Map> */}
      {/* <p>place for a display address location on map view</p>  */}
      <MapContainer center={position} zoom={13} scrollWheelZoom={false}>
-   <TileLayer
-   attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-  />
-  <Marker position={position}>
-     <Popup>
+      <TileLayer
+        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+    <Marker position={position}>
+      <Popup>
        A pretty CSS3 popup. <br /> Easily customizable.
-   </Popup>
+      </Popup>
    </Marker>
  </MapContainer>
   </section> 
