aadd0e24a6fac23d2e5b6e1bacb28e657a2ea570
MichalDziedzic <michaldziedzic8@gmail.com>
2021-07-21 15:42:20 +0200

    add data to map

--- app/src/App.js
+++ app/src/App.js
@@ -1,28 +1,39 @@
 import React, {useState,useEffect} from "react";
 import axios from 'axios';
 import './App.css';
-import { MapContainer, TileLayer,Marker,Popup} from 'react-leaflet'
-
 import HeaderTitle from'./components/Header';
 import InputTracker from'./components/InputTracker';
 import ResultTracker from'./components/ResultTracker';
-// import Map from './components/Map';
+ import Map from './components/Map';
 
  const App=()=> {
-     const [ipUserInfo ,setIpUserInfo]=useState([]);
-    const apiKey=`at_3IpXga8xWzOhB4m0Jta5UyQgtFgLW`;
+      const [ipUserLocation ,setIpUserLocation]=useState([]);
+      const [isp,setIsp]=useState('');
+    const [lat,setLat]=useState('');
+    const [lng,setLng]=useState('');
+    const apiKey=`at_ZWirdu7GQBgYHHcfroKKUmHvLAbRN`;
+    
+   /*
+      ip adreess | location(Brooklyn,NY,postcode) |timezone(UTC-05:00) | ISP (spaceX starlink)
+
+   */
 
   useEffect(()=>{
-    axios.get(`https://geo.ipify.org/api/v1?apiKey=${apiKey}&ipAddress=8.8.8.8`)
-    .then(response=>setIpUserInfo(response))
+    axios.get(`https://geo.ipify.org/api/v1?apiKey=${apiKey}&ipAddress=176.111.123.203`)
+    .then((response)=>{
+      setLat(response.data.location.lat);
+      setLng(response.data.location.lng);
+       setIpUserLocation(response.data.location);
+       setIsp(response.data.isp);
+    })
+    .catch(err => { console.log(err)})
 
-  },)
+  },[])
+
+  
   const projectAdress = `https://www.frontendmentor.io?ref=challenge`;
    const author= `Michał Dziedzic`;
-  const position = [51.505, -0.09]
-
-
-
+ 
    return(
   <div>
    <header>
@@ -31,19 +42,8 @@ import ResultTracker from'./components/ResultTracker';
     <ResultTracker/>
   </header>
   <section> 
-      {/* <Map></Map> */}
-     {/* <p>place for a display address location on map view</p>  */}
-     <MapContainer center={position} zoom={13} scrollWheelZoom={false}>
-      <TileLayer
-        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-    <Marker position={position}>
-      <Popup>
-       A pretty CSS3 popup. <br /> Easily customizable.
-      </Popup>
-   </Marker>
- </MapContainer>
+       <Map lat={lat} lng={lng} ></Map> 
+ 
   </section> 
   <footer>
     Challenge by <a href={projectAdress} >Frontend Mentor</a>. 
